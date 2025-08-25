5409927e9d489c83d4ac464345acf319a3c4cf32
Rohithn7274 <eng24am0265@dsu.edu.in>
2025-08-25 22:14:47 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout << "Enter the first number: ";
+    cin >> a;
+    cout << "Enter the second number: ";
+    cin >> b;
+    cout << "Sum is: " << (a+b) << endl;
+    cout << "Diff is: " << (a-b) << endl;
+    cout << "Pro is: " << (a*b) << endl;
+    cout << "Division is: " << (a/b) << endl;
+    cout << "Rem is: " << (a%b) << endl;
+    return 0;
+}
