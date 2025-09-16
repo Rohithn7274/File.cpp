3afc3680bbe23f18254078ecaa74ef0144e89db6
Rohithn7274 <eng24am0265@dsu.edu.in>
2025-09-16 22:36:04 +0530

    Add files via upload

---
+++ do.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1,n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    do{
+        cout<<i<<" ";
+        i++;
+    }while(i<=n);
+    return 0;
+    }
